c2f140dfc93fd9388e377a961e5dbc8c165273ac
mustafaaldow <244281079+mustafaaldow233-ops@users.noreply.github.com>
2025-11-16 22:09:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Website Loaded Successfully");
+document.addEventListener("DOMContentLoaded", () => {
+    if (localStorage.getItem("lang") === "en") {
+        setLang("en");
+    } else {
+        setLang("ar");
+    }
+});
